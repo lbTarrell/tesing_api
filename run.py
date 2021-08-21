d4c5200f574c42b08073916d6a28617955399346
lbTarrell <62875506+lbTarrell@users.noreply.github.com>
2021-08-21 20:22:28 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -28,4 +28,5 @@ def postInput():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    from os import environ
+    app.run(debug=False, port=environ.get("PORT", 5000), processes=2)
